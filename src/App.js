d3322b1db34a7e6b6f5cccbf16c5abeb28d0f2bf
Heneros <punk60826@gmail.com>
2022-08-19 01:06:08 +0300

    added styles and created function handleToggleTodo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery, useMutation } from '@apollo/client';
 
 const GET_TODOS = gql`
 query getTodos{
@@ -11,8 +11,26 @@ query getTodos{
 }
 `;
 
+const TOGGLE_TODO = gql`
+ mutation toggleTodo($id: uuid!, $done: Boolean!){
+  update_todos(where: {id: {_eq: $id}}, _set:{done: $done} ){
+    returning{
+      done
+      id
+      text
+    }
+  }
+ }
+`;
+
 function App() {
   const { data, loading, error } = useQuery(GET_TODOS);
+  const [toggleTodo] = useMutation(TOGGLE_TODO);
+
+  async function handleToggleTodo({ id, done }) {
+   const data = await toggleTodo({ variables: { id, done: !done } })
+   console.log(data);
+  }
 
   if (loading) return <div>Loading... </div>
   if (error) return <div>Error fetching data</div>
@@ -20,19 +38,23 @@ function App() {
     <h1 className='f2-1'>React Checklist</h1>
     <form className='mb3'>
       <input
-      className='pa2 f4 b--dashed'
+        className='pa2 f4 b--dashed'
         type="text"
         placeholder="write your todo"
       />
-      <button type="submit">Create</button>
+      <button
+        className='pa2 f4 bg-green'
+        type="submit">Create</button>
     </form>
-    <div>
+    <div className='flex items-center justify-center flex-column'>
       {data.todos.map(todo => (
-        <p key={todo.id}>
-          <span>
+        <p onDoubleClick={() => handleToggleTodo(todo)} key={todo.id}>
+          <span className={`pointer list pa1 f3 ${todo.done && "strike"}`}>
             {todo.text}
           </span>
-          <button>&times;</button>
+          <button className='bg-transparent bn f4'>
+            <span className='red'>&times;</span>
+          </button>
         </p>
       ))}
     </div>
