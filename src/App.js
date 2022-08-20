fd25416305f87ef1b97cc0cb9bc9d3d384a6038d
Heneros <punk60826@gmail.com>
2022-08-20 19:00:38 +0300

    ADD_TODO created

--- src/App.js
+++ src/App.js
@@ -23,24 +23,52 @@ const TOGGLE_TODO = gql`
  }
 `;
 
+const ADD_TODO = gql`
+  mutation addTodo($text: String!){
+    insert_todos(objects: {text: $text}){
+      returning{
+        done
+        id
+        text
+      }
+    }
+  }
+`;
+
+
+
 function App() {
+  const [todoText, setTodoText] = React.useState('');
   const { data, loading, error } = useQuery(GET_TODOS);
   const [toggleTodo] = useMutation(TOGGLE_TODO);
+  const [addTodo] = useMutation(ADD_TODO)
 
   async function handleToggleTodo({ id, done }) {
-   const data = await toggleTodo({ variables: { id, done: !done } })
-   console.log(data);
+    const data = await toggleTodo({ variables: { id, done: !done } })
+    console.log('toggle todo' , data);
+  }
+
+  async function handleAddTodo(event) {
+    event.preventDefault();
+    if (!todoText.trim()) return; ///If not empty
+    const data = await addTodo({ variables: { text: todoText } });
+    console.log('add todo', data);
+    setTodoText('');/// clear text after add todo
   }
 
   if (loading) return <div>Loading... </div>
   if (error) return <div>Error fetching data</div>
   return (<div className="vh-100 code flex flex-column items-center bg-purple white pa3 fl-1" >
     <h1 className='f2-1'>React Checklist</h1>
-    <form className='mb3'>
+    <form
+      onSubmit={handleAddTodo}
+      className='mb3'>
       <input
         className='pa2 f4 b--dashed'
         type="text"
         placeholder="write your todo"
+        onChange={event => setTodoText(event.target.value)}
+        value={todoText}
       />
       <button
         className='pa2 f4 bg-green'
@@ -48,6 +76,7 @@ function App() {
     </form>
     <div className='flex items-center justify-center flex-column'>
       {data.todos.map(todo => (
+        ///onDoubleClick cross text
         <p onDoubleClick={() => handleToggleTodo(todo)} key={todo.id}>
           <span className={`pointer list pa1 f3 ${todo.done && "strike"}`}>
             {todo.text}
